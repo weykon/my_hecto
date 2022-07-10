0ef0e6b91bcf3f1aeee0dba985efc24d714f723c
weykon <635883092@qq.com>
2022-07-11 07:31:52 +0800

    4

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,8 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+mod editor;
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
+    let editor = Editor::default();
 
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-                if b == to_ctrl_byte('b') {
-                    break;
-                }
-            }
-            Err(err) => die(err),
-        }
-    }
+    editor.run();
 }
