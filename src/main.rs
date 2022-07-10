69ff56c30e7a835fff619c1cd45b99d8153c8c5e
weykon <635883092@qq.com>
2022-07-10 19:59:19 +0800

    2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use std::io::{self, Read , stdout};
+use std::io::{self, stdout, Read};
 use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
-        if c == 'q' {
+        let b = b.unwrap();
+        let c = b as char;
+
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+
+        if b == to_ctrl_byte('s') {
             break;
         }
     }
